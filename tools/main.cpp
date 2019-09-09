789d869f6d008c431c96ae87fdd2f5c2fa3273b4
ll-eleven <qq1503038761@163.com>
2019-09-09 09:26:03 +0800

    add deal with multi-screen

--- tools/main.cpp
+++ tools/main.cpp
@@ -12,14 +12,18 @@ int main(int argc, char* argv[])
     pw = new PowerWin(false);
 
     //加载qss文件
-    QFile qss(":/powerwin.qss");
+    QFile qss("../data/powerwin.qss");
     qss.open(QFile::ReadOnly);
     a.setStyleSheet(qss.readAll());
     qss.close();
 
+    QRect screen = QApplication::desktop()->screenGeometry(QCursor::pos());
+    int xx = screen.x();
+    int yy = screen.y();//取得当前鼠标所在屏幕的最左，上坐标。
+
     pw->setWindowFlag(Qt::WindowStaysOnTopHint);//设置为顶层窗口，无法被切屏
     pw->showFullScreen();
-    pw->move((QApplication::desktop()->width() - pw->width())/2,(QApplication::desktop()->height()- pw->height())/2);
+    pw->move(xx+(screen.width() - pw->width())/2,yy+(screen.height()- pw->height())/2);
 
     return a.exec();
 }
