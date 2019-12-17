291ac5b8b4d6f8aa1207bd0b383d25264d92bc30
handsome-feng <jianfengli@ubuntukylin.com>
2019-12-17 15:47:45 +0800

    Update CMakeLists and other files for i18n

---
+++ translations/bo.ts
@@ -0,0 +1,70 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>PowerWin</name>
+    <message>
+        <location filename="../tools/powerwin.cpp" line="54"/>
+        <source>suspend</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../tools/powerwin.cpp" line="60"/>
+        <source>hibernate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../tools/powerwin.cpp" line="66"/>
+        <source>switchuser</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../tools/powerwin.cpp" line="76"/>
+        <source>reboot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../tools/powerwin.cpp" line="77"/>
+        <source>shutdown</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../tools/powerwin.cpp" line="78"/>
+        <source>logout</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../tools/powerwin.cpp" line="86"/>
+        <source>cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QApplication</name>
+    <message>
+        <location filename="../tools/main.cpp" line="19"/>
+        <source>UKUI session tools, show the shutdown dialog without any arguments.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../tools/main.cpp" line="26"/>
+        <source>Logout this computer.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../tools/main.cpp" line="28"/>
+        <source>Shutdown this computer.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../tools/main.cpp" line="30"/>
+        <source>Switch the user of this computer.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../tools/main.cpp" line="32"/>
+        <source>Restart this computer.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
