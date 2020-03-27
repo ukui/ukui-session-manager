22978904a0c344af5d6aa2f54a96d15b8286d520
ll-eleven <qq1503038761@163.com>
2020-03-27 14:42:17 +0800

    fix the bug while gsettings has been deleted

--- ukui-session/sessionapplication.cpp
+++ ukui-session/sessionapplication.cpp
@@ -27,41 +27,65 @@
 #include <QMediaPlayer>
 #include <QDesktopWidget>
 
+#define SESSION_DEFAULT_SETTINGS "org.ukui.session"
+#define QT5_UKUI_STYLE "org.ukui.style"
+
 void SessionApplication::InitialEnvironment()
 {
-    int gdk_scale;
-    int qt_scale_factor;
-    QDesktopWidget *desktop = QApplication::desktop();
-    qDebug()<< "Screen-height is"<<desktop->height()<<",Screnn-width is"<<desktop->width();
-    bool Hidpi = gs->get("hidpi").toBool();
-    qDebug()<< "Hidpi is "<<Hidpi;
-    if(Hidpi){
-        gdk_scale = gs->get("gdk-scale").toInt();
-        qt_scale_factor = gs->get("qt-scale-factor").toInt();
-    }else{
-        int i = 1;
-        if(desktop->height() >= 2000)
-            i = 2;
-        gdk_scale = i;
-        qt_scale_factor = i;
-    }
-    QString qt1 = QString::number(qt_scale_factor);
-    QByteArray qt2;
-    qt2.append(qt1);
-    QString gdk1 = QString::number(gdk_scale);
-    QByteArray gdk2;
-    gdk2.append(gdk1);
+    QByteArray gdk_scale_QB;
+    QByteArray qt_scale_factor_QB;
+    if(gsettings_usable){
+        int gdk_scale;
+        int qt_scale_factor;
+        QDesktopWidget *desktop = QApplication::desktop();
+        qDebug()<< "Screen-height is"<<desktop->height()<<",Screnn-width is"<<desktop->width();
+        bool Hidpi = gs->get("hidpi").toBool();
+        qDebug()<< "Hidpi is "<<Hidpi;
+        if(Hidpi){
+            gdk_scale = gs->get("gdk-scale").toInt();
+            qt_scale_factor = gs->get("qt-scale-factor").toInt();
+        }else{
+            int i = 1;
+            if(desktop->height() >= 2000)
+                i = 2;
+            gdk_scale = i;
+            qt_scale_factor = i;
+        }
+        //转换类型
+        QString qt_scale_factor_QS = QString::number(qt_scale_factor);
+        qt_scale_factor_QB.append(qt_scale_factor_QS);
 
-    qDebug()<< "gdk_scale"<<gdk2<<"qt_scale_factor"<<qt2;
+        QString gdk_scale_QS = QString::number(gdk_scale);
+        gdk_scale_QB.append(gdk_scale_QS);
+    }else{
+        //设为默认值
+        gdk_scale_QB = "1";
+        qt_scale_factor_QB = "1";
+    }
+
+    //检查qt主题是否安装
+    const QByteArray qt_style(QT5_UKUI_STYLE);
+    QByteArray QT_QPA_PLATFORMTHEME;
+    if(QGSettings::isSchemaInstalled(qt_style)) {
+        QT_QPA_PLATFORMTHEME = "ukui";
+    }else{
+        QT_QPA_PLATFORMTHEME = "gtk2";
+    }
+
+    qDebug()<< "gdk_scale"<<gdk_scale_QB<<"qt_scale_factor"<<qt_scale_factor_QB;
     qputenv("XDG_CURRENT_DESKTOP","UKUI");
-    qputenv("GDK_SCALE",gdk2);
-    qputenv("QT_SCALE_FACTOR",qt2);
+    qputenv("GDK_SCALE",gdk_scale_QB);
+    qputenv("QT_SCALE_FACTOR",qt_scale_factor_QB);
     qputenv("QT_AUTO_SCRENN_SET_FACTOR","0");
+    qputenv("QT_QPA_PLATFORMTHEME",QT_QPA_PLATFORMTHEME);
 }
 
 void SessionApplication::updatevalue(){
-    const int time = gs->get("idle-delay").toInt() * 60;
-    mIdleWatcher->reset(time);
+    if(gsettings_usable){
+        const int time = gs->get("idle-delay").toInt() * 60;
+        mIdleWatcher->reset(time);
+    }else
+        return;
 }
 
 void SessionApplication::registerDBus()
@@ -78,8 +102,13 @@ void SessionApplication::registerDBus()
                     << "/org/gnome/SessionManager";
     }
 
-    const int timeout = gs->get("idle-delay").toInt() * 60;
-    connect(gs,&QGSettings::changed,this,&SessionApplication::updatevalue);
+    int timeout;
+    if(gsettings_usable){
+        timeout = gs->get("idle-delay").toInt() * 60;
+        connect(gs,&QGSettings::changed,this,&SessionApplication::updatevalue);
+    }else{
+        timeout = 5 * 60;
+    }
     mIdleWatcher = new IdleWatcher(timeout);
     new IdleDBusAdaptor(mIdleWatcher);
     if (!dbus.registerObject("/org/gnome/SessionManager/Presence", mIdleWatcher))
@@ -92,7 +121,15 @@ void SessionApplication::registerDBus()
 SessionApplication::SessionApplication(int& argc, char** argv) :
     QApplication(argc, argv)
 {
-    gs = new QGSettings("org.ukui.session","/org/ukui/desktop/session/",this);
+    const QByteArray id(SESSION_DEFAULT_SETTINGS);
+    if(QGSettings::isSchemaInstalled(id)) {
+        gsettings_usable = true;
+        gs = new QGSettings("org.ukui.session","/org/ukui/desktop/session/",this);
+    }else{
+        //gsetting安装失败，或无法获取，设置默认值
+        qDebug() << "从gsettings 中或取值失败，设置默认值";
+        gsettings_usable = false;
+    }
 
     InitialEnvironment();
 
