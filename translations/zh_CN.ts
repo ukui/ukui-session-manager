2021b64587e66b5dcba4cf268386e96e4fd9aa1f
ll-eleven <qq1503038761@163.com>
2020-04-08 19:25:04 +0800

    Modify the capitalization of the first letter of the option, add the shutdown interface to shield the keyboard, capture the esc key and press to exit

--- translations/zh_CN.ts
+++ translations/zh_CN.ts
@@ -1,65 +1,65 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh_CN">
+<TS version="2.1" language="en_US">
 <context>
     <name>MainWindow</name>
     <message>
         <location filename="../tools/mainwindow.ui" line="14"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../tools/mainwindow.ui" line="57"/>
-        <source>suspend</source>
-        <translation>锁定</translation>
+        <location filename="../tools/mainwindow.ui" line="61"/>
+        <source>Suspend</source>
+        <translation>挂起</translation>
     </message>
     <message>
-        <location filename="../tools/mainwindow.ui" line="105"/>
-        <source>switchuser</source>
+        <location filename="../tools/mainwindow.ui" line="113"/>
+        <source>Switch User</source>
         <translation>切换用户</translation>
     </message>
     <message>
-        <location filename="../tools/mainwindow.ui" line="153"/>
-        <source>logout</source>
+        <location filename="../tools/mainwindow.ui" line="165"/>
+        <source>Log Out</source>
         <translation>注销</translation>
     </message>
     <message>
-        <location filename="../tools/mainwindow.ui" line="201"/>
-        <source>reboot</source>
+        <location filename="../tools/mainwindow.ui" line="217"/>
+        <source>Restart</source>
         <translation>重启</translation>
     </message>
     <message>
-        <location filename="../tools/mainwindow.ui" line="249"/>
-        <source>shutdown</source>
+        <location filename="../tools/mainwindow.ui" line="269"/>
+        <source>Power Off</source>
         <translation>关机</translation>
     </message>
 </context>
 <context>
     <name>QApplication</name>
     <message>
-        <location filename="../tools/main.cpp" line="44"/>
+        <location filename="../tools/main.cpp" line="43"/>
         <source>UKUI session tools, show the shutdown dialog without any arguments.</source>
-        <translation type="unfinished"></translation>
+        <translation>UKUI会话工具，不带参数时显示关机界面。</translation>
     </message>
     <message>
-        <location filename="../tools/main.cpp" line="51"/>
+        <location filename="../tools/main.cpp" line="50"/>
         <source>Logout this computer.</source>
-        <translation>注销此电脑</translation>
+        <translation>注销。</translation>
     </message>
     <message>
-        <location filename="../tools/main.cpp" line="53"/>
+        <location filename="../tools/main.cpp" line="52"/>
         <source>Shutdown this computer.</source>
-        <translation>关闭此电脑</translation>
+        <translation>关机。</translation>
     </message>
     <message>
-        <location filename="../tools/main.cpp" line="55"/>
+        <location filename="../tools/main.cpp" line="54"/>
         <source>Switch the user of this computer.</source>
-        <translation>切换电脑的用户</translation>
+        <translation>切换用户。</translation>
     </message>
     <message>
-        <location filename="../tools/main.cpp" line="57"/>
+        <location filename="../tools/main.cpp" line="56"/>
         <source>Restart this computer.</source>
-        <translation>重启此电脑</translation>
+        <translation>重启。</translation>
     </message>
 </context>
 </TS>
