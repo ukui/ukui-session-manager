ed4cba4cc192e46580d690a471e80430a28c44ff
Aylinux <cihanalk@gmail.com>
2020-05-13 10:34:38 +0300

    Create tr.ts

---
+++ translations/tr.ts
@@ -0,0 +1,65 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../tools/mainwindow.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../tools/mainwindow.ui" line="61"/>
+        <source>Suspend</source>
+        <translation>Askıya Al</translation>
+    </message>
+    <message>
+        <location filename="../tools/mainwindow.ui" line="113"/>
+        <source>Switch User</source>
+        <translation>Kullanıcı Değiştir</translation>
+    </message>
+    <message>
+        <location filename="../tools/mainwindow.ui" line="165"/>
+        <source>Log Out</source>
+        <translation>Çık</translation>
+    </message>
+    <message>
+        <location filename="../tools/mainwindow.ui" line="217"/>
+        <source>Restart</source>
+        <translation>Yeniden Başlat</translation>
+    </message>
+    <message>
+        <location filename="../tools/mainwindow.ui" line="269"/>
+        <source>Power Off</source>
+        <translation>Bilgisayarı Kapat</translation>
+    </message>
+</context>
+<context>
+    <name>QApplication</name>
+    <message>
+        <location filename="../tools/main.cpp" line="43"/>
+        <source>UKUI session tools, show the shutdown dialog without any arguments.</source>
+        <translation>UKUI oturum araçları, herhangi bir argüman olmadan kapatma iletişim kutusunu gösterir</translation>
+    </message>
+    <message>
+        <location filename="../tools/main.cpp" line="50"/>
+        <source>Logout this computer.</source>
+        <translation>Bu bilgisayardan çık.</translation>
+    </message>
+    <message>
+        <location filename="../tools/main.cpp" line="52"/>
+        <source>Shutdown this computer.</source>
+        <translation>Bu bilgisayarı kapat.</translation>
+    </message>
+    <message>
+        <location filename="../tools/main.cpp" line="54"/>
+        <source>Switch the user of this computer.</source>
+        <translation>Bu bilgisayardaki kullanıcıyı değiştir.</translation>
+    </message>
+    <message>
+        <location filename="../tools/main.cpp" line="56"/>
+        <source>Restart this computer.</source>
+        <translation>Bu bilgisayarı yeniden başlat.</translation>
+    </message>
+</context>
+</TS>
