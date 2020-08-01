f5acf41ae951734013a94f4bf88d0bb71c6c01fd
Aylinux <cihanalk@gmail.com>
2020-08-01 09:39:31 +0300

    Update tr.ts (#39)

--- translations/tr.ts
+++ translations/tr.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="tr">
+<TS version="2.1" language="tr_TR">
 <context>
     <name>MainWindow</name>
     <message>
@@ -10,56 +10,76 @@
     </message>
     <message>
         <location filename="../tools/mainwindow.ui" line="61"/>
-        <source>Suspend</source>
-        <translation>Askıya Al</translation>
-    </message>
-    <message>
-        <location filename="../tools/mainwindow.ui" line="113"/>
         <source>Switch User</source>
         <translation>Kullanıcı Değiştir</translation>
     </message>
     <message>
-        <location filename="../tools/mainwindow.ui" line="165"/>
-        <source>Log Out</source>
-        <translation>Çık</translation>
+        <location filename="../tools/mainwindow.ui" line="113"/>
+        <source>Hebernate</source>
+        <translation>Hazırda Beklet</translation>
     </message>
     <message>
-        <location filename="../tools/mainwindow.ui" line="217"/>
-        <source>Restart</source>
+        <location filename="../tools/mainwindow.ui" line="239"/>
+        <source>Suspend</source>
+        <translation>Askıya Al</translation>
+    </message>
+    <message>
+        <location filename="../tools/mainwindow.ui" line="291"/>
+        <source>Logout</source>
+        <translation>Çıkış</translation>
+    </message>
+    <message>
+        <location filename="../tools/mainwindow.ui" line="343"/>
+        <source>Reboot</source>
         <translation>Yeniden Başlat</translation>
     </message>
     <message>
-        <location filename="../tools/mainwindow.ui" line="269"/>
-        <source>Power Off</source>
-        <translation>Bilgisayarı Kapat</translation>
+        <location filename="../tools/mainwindow.ui" line="395"/>
+        <source>Shut Down</source>
+        <translation>Kapat</translation>
+    </message>
+    <message>
+        <location filename="../tools/mainwindow.ui" line="447"/>
+        <source>Lock Screen</source>
+        <translation>Ekranı Kilitle</translation>
     </message>
 </context>
 <context>
     <name>QApplication</name>
     <message>
-        <location filename="../tools/main.cpp" line="43"/>
+        <location filename="../tools/main.cpp" line="84"/>
         <source>UKUI session tools, show the shutdown dialog without any arguments.</source>
-        <translation>UKUI oturum araçları, herhangi bir argüman olmadan kapatma iletişim kutusunu gösterir</translation>
+        <translation>UKUI oturum araçları, herhangi bir argüman olmadan kapatma iletişim kutusunu gösterir.</translation>
     </message>
     <message>
-        <location filename="../tools/main.cpp" line="50"/>
-        <source>Logout this computer.</source>
-        <translation>Bu bilgisayardan çık.</translation>
-    </message>
-    <message>
-        <location filename="../tools/main.cpp" line="52"/>
-        <source>Shutdown this computer.</source>
-        <translation>Bu bilgisayarı kapat.</translation>
-    </message>
-    <message>
-        <location filename="../tools/main.cpp" line="54"/>
+        <location filename="../tools/main.cpp" line="91"/>
         <source>Switch the user of this computer.</source>
         <translation>Bu bilgisayardaki kullanıcıyı değiştir.</translation>
     </message>
     <message>
-        <location filename="../tools/main.cpp" line="56"/>
+        <location filename="../tools/main.cpp" line="93"/>
+        <source>Hibernate this computer.</source>
+        <translation>Bu bilgisayarı hazırda beklet.</translation>
+    </message>
+    <message>
+        <location filename="../tools/main.cpp" line="95"/>
+        <source>Suspend this computer.</source>
+        <translation>Bu bilgisayarı askıya al.</translation>
+    </message>
+    <message>
+        <location filename="../tools/main.cpp" line="97"/>
+        <source>Logout this computer.</source>
+        <translation>Bu bilgisayardan çık.</translation>
+    </message>
+    <message>
+        <location filename="../tools/main.cpp" line="99"/>
         <source>Restart this computer.</source>
         <translation>Bu bilgisayarı yeniden başlat.</translation>
+    </message>
+    <message>
+        <location filename="../tools/main.cpp" line="101"/>
+        <source>Shutdown this computer.</source>
+        <translation>Bu bilgisayarı kapat.</translation>
     </message>
 </context>
 </TS>
