3aaed56115343f066fdb0bac986ad2acd28320b9
ll-eleven <qq1503038761@163.com>
2021-04-21 09:27:55 +0800

    update translations

--- translations/zh_CN.ts
+++ translations/zh_CN.ts
@@ -43,104 +43,134 @@
         <source>Lock Screen</source>
         <translation>锁屏</translation>
     </message>
+    <message>
+        <location filename="../tools/mainwindow.ui" line="529"/>
+        <source>cancel</source>
+        <translation>取消</translation>
+    </message>
+    <message>
+        <location filename="../tools/mainwindow.ui" line="542"/>
+        <source>confirm</source>
+        <translation>确认</translation>
+    </message>
+    <message>
+        <location filename="../tools/mainwindow.ui" line="559"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>QApplication</name>
     <message>
-        <location filename="../tools/main.cpp" line="211"/>
+        <location filename="../tools/main.cpp" line="298"/>
         <source>UKUI session tools, show the shutdown dialog without any arguments.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../tools/main.cpp" line="218"/>
+        <location filename="../tools/main.cpp" line="305"/>
         <source>Switch the user of this computer.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../tools/main.cpp" line="220"/>
+        <location filename="../tools/main.cpp" line="307"/>
         <source>Hibernate this computer.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../tools/main.cpp" line="222"/>
+        <location filename="../tools/main.cpp" line="309"/>
         <source>Suspend this computer.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../tools/main.cpp" line="224"/>
+        <location filename="../tools/main.cpp" line="311"/>
         <source>Logout this computer.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../tools/main.cpp" line="226"/>
+        <location filename="../tools/main.cpp" line="313"/>
         <source>Restart this computer.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../tools/main.cpp" line="228"/>
+        <location filename="../tools/main.cpp" line="315"/>
         <source>Shutdown this computer.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../tools/main.cpp" line="230"/>
+        <location filename="../tools/main.cpp" line="317"/>
         <source>A window above the desktop.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../tools/mainwindow.cpp" line="124"/>
+        <location filename="../tools/mainwindow.cpp" line="148"/>
+        <source>Multiple users are logged in at the same time.Are you sure you want to close this system?</source>
+        <translation>同时有多个用户处于登录状态，你确定要退出系统吗？</translation>
+    </message>
+    <message>
+        <location filename="../tools/mainwindow.cpp" line="166"/>
         <source>(user),ukui-control-center is performing a system update or package installation.</source>
-        <translation>（用户），控制面板正在执行系统升级或软件包安装</translation>
+        <translation>（用户），控制面板正在进行系统升级或软件包安装。</translation>
     </message>
     <message>
-        <location filename="../tools/mainwindow.cpp" line="125"/>
+        <location filename="../tools/mainwindow.cpp" line="167"/>
         <source>(user),yhkylin-backup-tools is performing a system backup or restore.</source>
-        <translation>（用户），麒麟备份还原工具正在进行系统备份或还原</translation>
+        <translation>（用户），麒麟备份还原工具正在进行备份或还原。</translation>
     </message>
     <message>
-        <location filename="../tools/mainwindow.cpp" line="126"/>
+        <location filename="../tools/mainwindow.cpp" line="168"/>
         <source>For system security,Reboot、Shutdown、Logout and Hibernate are temporarily unavailable.</source>
-        <translation>为了系统的安全，重启、关机、注销和休眠功能暂时不可用</translation>
+        <translation>为了系统安全，重启、关机、注销和休眠功能暂时不可用。</translation>
     </message>
     <message>
-        <location filename="../tools/mainwindow.cpp" line="127"/>
+        <location filename="../tools/mainwindow.cpp" line="169"/>
         <source>For system security,Reboot、Shutdown and Hibernate are temporarily unavailable.</source>
-        <translation>为了系统的安全，重启、关机和休眠功能暂时不可用</translation>
+        <translation>为了系统安全，重启、关机和休眠功能暂时不可用。</translation>
     </message>
 </context>
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../tools/main.cpp" line="52"/>
+        <location filename="../tools/main.cpp" line="82"/>
+        <source>Multiple users are logged in at the same time.Are you sure you want to close this system?</source>
+        <translation>同时有多个用户处于登录状态，你确定要退出系统吗？</translation>
+    </message>
+    <message>
+        <location filename="../tools/main.cpp" line="83"/>
+        <source>cancel</source>
+        <translation>取消</translation>
+    </message>
+    <message>
+        <location filename="../tools/main.cpp" line="84"/>
+        <source>confirm</source>
+        <translation>确认</translation>
+    </message>
+    <message>
+        <location filename="../tools/main.cpp" line="100"/>
         <source>notice</source>
         <translation>提示</translation>
     </message>
     <message>
-        <location filename="../tools/main.cpp" line="53"/>
+        <location filename="../tools/main.cpp" line="101"/>
         <source>System update or package installation in progress,this function is temporarily unavailable.</source>
-        <translation>系统升级或软件包安装正在进行中，此功能暂时不可用</translation>
+        <translation>正在进行系统升级或软件包安装，这个功能暂时不可用。</translation>
     </message>
     <message>
-        <location filename="../tools/main.cpp" line="54"/>
+        <location filename="../tools/main.cpp" line="102"/>
         <source>System backup or restore in progress,this function is temporarily unavailable.</source>
-        <translation>系统备份或系统还原正在进行中，此功能暂时不可用</translation>
+        <translation>正在进行系统备份或还原，这个功能暂时不可用。</translation>
     </message>
     <message>
-        <location filename="../tools/powerprovider.cpp" line="52"/>
-        <source>conform</source>
-        <translation>确认</translation>
-    </message>
-    <message>
-        <location filename="../tools/powerprovider.cpp" line="53"/>
+        <location filename="../tools/powerprovider.cpp" line="107"/>
         <source>some applications are running and they dont want you to do this.</source>
-        <translation>一些功能正在进行中，并且他们不希望你执行它</translation>
+        <translation>一些程序正在运行，而且它不希望你继续该操作。</translation>
     </message>
     <message>
-        <location filename="../tools/powerprovider.cpp" line="54"/>
+        <location filename="../tools/powerprovider.cpp" line="108"/>
         <source>Still to do!</source>
-        <translation>任然执行！</translation>
+        <translation>仍然执行！</translation>
     </message>
     <message>
-        <location filename="../tools/powerprovider.cpp" line="55"/>
+        <location filename="../tools/powerprovider.cpp" line="109"/>
         <source>give up</source>
         <translation>放弃</translation>
     </message>
