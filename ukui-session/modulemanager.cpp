504913137cda74a75af13cfeb479880331b95f01
zhouxiaofeng <zhouxiaofeng@kylinos.cn>
2021-09-14 20:55:28 +0800

    Fix:青松S4崩溃

--- ukui-session/modulemanager.cpp
+++ ukui-session/modulemanager.cpp
@@ -20,6 +20,7 @@
 #include "modulemanager.h"
 #include "ukuimodule.h"
 #include "idlewatcher.h"
+#include "musicplayer.h"
 
 #include <QCoreApplication>
 #include "xdgautostart.h"
@@ -54,21 +55,39 @@ void ModuleManager::playBootMusic(bool arg){
             free(gset);
             return;
         }
-        player = new QMediaPlayer;
-        connect(player,SIGNAL(stateChanged(QMediaPlayer::State)),this,SLOT(stateChanged(QMediaPlayer::State)));
-        player->setVolume(40);
-        if(arg){
-            play_music = gset->get("startup-music").toBool();
-            if (play_music) {
-                player->setMedia(QUrl::fromLocalFile("/usr/share/ukui/ukui-session-manager/startup.wav"));
-                player->play();
+//        player = new QMediaPlayer;
+//        connect(player,SIGNAL(stateChanged(QMediaPlayer::State)),this,SLOT(stateChanged(QMediaPlayer::State)));
+//        player->setVolume(40);
+//        if(arg){
+//            play_music = gset->get("startup-music").toBool();
+//            if (play_music) {
+//                player->setMedia(QUrl::fromLocalFile("/usr/share/ukui/ukui-session-manager/startup.wav"));
+//                player->play();
+//            }
+//        }else{
+//            play_music = gset->get("weakup-music").toBool();
+//            if (play_music) {
+//                player->setMedia(QUrl::fromLocalFile("/usr/share/ukui/ukui-session-manager/weakup.wav"));
+//                player->play();
+//            }
+//        }
+
+        play_music = gset->get("startup-music").toBool();
+        if (play_music) {
+            if (arg) {
+                musicplayer *player = new musicplayer;
+                player->setVolumn(40);
+                player->setSource(QLatin1String("/usr/share/ukui/ukui-session-manager/startup.wav"));
+                connect(player, &musicplayer::playFinished, player, &QObject::deleteLater);
+                player->start();
+            } else {
+                musicplayer *player = new musicplayer;
+                player->setVolumn(40);
+                player->setSource(QLatin1String("/usr/share/ukui/ukui-session-manager/weakup.wav"));
+                connect(player, &musicplayer::playFinished, player, &QObject::deleteLater);
+                player->start();
             }
-        }else{
-            play_music = gset->get("weakup-music").toBool();
-            if (play_music) {
-                player->setMedia(QUrl::fromLocalFile("/usr/share/ukui/ukui-session-manager/weakup.wav"));
-                player->play();
-            }
+
         }
     }
 }
@@ -138,7 +157,6 @@ void ModuleManager::constructStartupList()
         const QGSettings* gs = new QGSettings(SESSION_REQUIRED_COMPONENTS,SESSION_REQUIRED_COMPONENTS_PATH,this);
         if(gs == NULL){
             qDebug()<<"QGSettings init error";
-            free(&gs);
             return;
         }
         window_manager = gs->get("windowmanager").toString() + ".desktop";
