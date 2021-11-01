9f7d80a6c85c8139b7f2e71b29d0763b6b8477bd
zxf <zhouxiaofeng@kylinos.cn>
2021-11-01 09:45:33 +0800

    恢复上次保存会话功能

--- ukui-session/modulemanager.cpp
+++ ukui-session/modulemanager.cpp
@@ -21,6 +21,7 @@
 #include "ukuimodule.h"
 #include "idlewatcher.h"
 #include "musicplayer.h"
+#include "ukuismserver.h"
 
 #include <QCoreApplication>
 #include "xdgautostart.h"
@@ -44,6 +45,8 @@
 
 #define SESSION_REQUIRED_COMPONENTS "org.ukui.session.required-components"
 #define SESSION_REQUIRED_COMPONENTS_PATH "/org/ukui/desktop/session/required-components/"
+
+extern UKUISMServer *theServer;
 
 void ModuleManager::playBootMusic(bool arg)
 {
@@ -463,6 +466,9 @@ void ModuleManager::timerUpdate(){
     for (XdgDesktopFileList::const_iterator i = mForceApplication.constBegin(); i != mForceApplication.constEnd(); ++i) {
         startProcess(*i, true);
     }
+
+    //加上恢复会话的部分
+    theServer->restoreSession();
 }
 
 void ModuleManager::startProcess(const XdgDesktopFile &file, bool required)
