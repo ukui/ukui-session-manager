21f30b4b8be9a03969402c58a47cd87350dea562
zhouxiaofeng <zhouxiaofeng@kylinos.cn>
2021-11-26 15:15:49 +0800

    格式调整

--- tools/mainwindow.h
+++ tools/mainwindow.h
@@ -33,7 +33,7 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow ,public QAbstractNativeEventFilter
+class MainWindow : public QMainWindow, public QAbstractNativeEventFilter
 {
     Q_OBJECT
 
