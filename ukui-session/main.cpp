af47eb09b8377247c6166ade4df17b9b0c52b2b6
zhouxiaofeng <zhouxiaofeng@kylinos.cn>
2021-12-09 16:56:52 +0800

    部分符号名修改，去掉与KDE有关的内容

--- ukui-session/main.cpp
+++ ukui-session/main.cpp
@@ -366,7 +366,7 @@ int main(int argc, char **argv)
 
     app.setQuitOnLastWindowClosed(false);
 //    UKUISMServer *server = new UKUISMServer;
-    UKUISMServer *server = getGlobalServer();
+    UKUISMServer *&server = getGlobalServer();
     IceSetIOErrorHandler(IoErrorHandler);
     server->restoreSession(QStringLiteral("saved at previous logout"));//恢复会话启动的窗管包含命令行参数
 //    server->startDefaultSession();//默认方式启动的窗管不含任何命令行参数
