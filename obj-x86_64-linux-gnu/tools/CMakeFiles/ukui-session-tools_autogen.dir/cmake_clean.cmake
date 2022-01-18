ca3e72253cbdd7c5369ed26fdfa3468f29ff76cb
tanjing <tanjing@kylinos.cn>
2022-01-18 13:41:22 +0800

    去掉多余文件

--- obj-x86_64-linux-gnu/tools/CMakeFiles/ukui-session-tools_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ukui-session-tools_autogen"
-  "ukui-session-tools_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ukui-session-tools_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
